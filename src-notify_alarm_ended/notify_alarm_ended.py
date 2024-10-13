ab401988ef1e793269b8673dc4acbb2f4e844348
SageTomas <tomerrd@gmail.com>
2024-10-13 14:11:29 +0300

    Added an EventBridge Rule to fire when the Natgateway Alarm returns to OK Added a Lambda function that is the target if said rule. This Lambda will fetch the tokenID field from the DynamoDB Item associated with the Nat Gateway ID that fired the event. If such a token exists, it will send a Task Successed message to the StepFunction so it would stop waiting for Timeout / Task Success

---
+++ src-notify_alarm_ended/notify_alarm_ended.py
@@ -0,0 +1,48 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.client('dynamodb')
+stepFunction = boto3.client('stepfunctions')
+
+# This function is triggered via EventBridge when the NatGateway alarm changes state to "OK".
+# It checks if the token is still define in DynamoDB for this Nat Gateway ID, and if it is
+# it will send a Task Success to the State Machine via the token
+def lambda_handler(event, context):
+    tableName = event['params']['tableName'];
+    ngwid = event['params']['ngwid']
+    stepFunction
+    
+    tokenID = getToken(tableName, ngwid)
+    
+    if (tokenID != None):
+        # Call Step Function with success and tokenID
+        print("Send Task Success to StepFunction")
+        stepFunction.send_task_success(
+            taskToken=tokenID,
+            output=json.dumps({"status": "success"})
+        )
+    else:
+        print("token was alreadyt removed")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Alarm Off')
+    }
+
+
+# Gets the step function token associated with a specific Nat Gateway ID
+def getToken(tableName, ngwid):
+    tokenID = None
+    
+    key = {'ngwid': {'S': ngwid}}
+    
+    try:
+        response = dynamodb.get_item(TableName=tableName, Key=key)
+        if 'Item' in response:
+            if 'tokenID' in response['Item']:
+                tokenID = response['Item']['tokenID']['S']
+    except ClientError as err:
+        print("Failed to get item from dyanmoDB with error: ", err.response["Error"]["Message"])
+
+    return tokenID
