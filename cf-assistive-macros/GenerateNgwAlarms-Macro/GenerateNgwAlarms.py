e0a27cdc8e2f3b925c43acccb3a31a1820127d2e
SageTomas <tomerrd@gmail.com>
2024-11-03 15:22:02 +0200

    Changed the function that generates the CloudWatch Alarm to generate a composite Alarm. It now uses two metrics - BytesInFromSource and BytesInFromDestination The Alarm itself uses an expressions that will be non zero if either of these two metrics pass the defined threshold

--- cf-assistive-macros/GenerateNgwAlarms-Macro/GenerateNgwAlarms.py
+++ cf-assistive-macros/GenerateNgwAlarms-Macro/GenerateNgwAlarms.py
@@ -1,26 +1,65 @@
 import json
 
-# A template to generate CloudWatch Alarms for Nat Gateways
-NatGatewayTemplate = """{
+# A template to generate Dual Metric CloudWatch Alarms for Nat Gateways
+NatGatewayDualMetricTemplate = """{
     "Type": "AWS::CloudWatch::Alarm",
     "Properties": {
-      "AlarmName": "NAME",
-      "ActionsEnabled": "false",
-      "MetricName": "BytesInFromDestination",
-      "Namespace": "AWS/NATGateway",
-      "Statistic": "Sum",
-      "Dimensions": [
-        {
-          "Name": "NatGatewayId",
-          "Value": "natgateway-id"
-        }
-      ],
-      "Period": 60,
-      "EvaluationPeriods": 3,
-      "DatapointsToAlarm": 1,
-      "Threshold": 100000,
-      "ComparisonOperator": "GreaterThanThreshold",
-      "TreatMissingData": "missing"
+        "AlarmName": "NAME",
+        "AlarmDescription": "Alarm to trigger VPC Flow Logs on a Nat Gateway",
+        "ActionsEnabled": false,
+        "OKActions": [],
+        "AlarmActions": [],
+        "InsufficientDataActions": [],
+        "Dimensions": [],
+        "EvaluationPeriods": 3,
+        "DatapointsToAlarm": 1,
+        "Threshold": 1,
+        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+        "TreatMissingData": "missing",
+        "Metrics": [
+            {
+                "Id": "e1",
+                "Label": "Bytes From Source or Destination above threshold",
+                "ReturnData": true,
+                "Expression": "IF(m1 > {}, 1, 0) + IF(m2 > {},1, 0)"
+            },
+            {
+                "Id": "m1",
+                "ReturnData": false,
+                "MetricStat": {
+                    "Metric": {
+                        "Namespace": "AWS/NATGateway",
+                        "MetricName": "BytesInFromSource",
+                        "Dimensions": [
+                            {
+                                "Name": "NatGatewayId",
+                                "Value": "natgateway-id"
+                            }
+                        ]
+                    },
+                    "Period": 60,
+                    "Stat": "Sum"
+                }
+            },
+            {
+                "Id": "m2",
+                "ReturnData": false,
+                "MetricStat": {
+                    "Metric": {
+                        "Namespace": "AWS/NATGateway",
+                        "MetricName": "BytesInFromDestination",
+                        "Dimensions": [
+                            {
+                                "Name": "NatGatewayId",
+                                "Value": "natgateway-id"
+                            }
+                        ]
+                    },
+                    "Period": 60,
+                    "Stat": "Sum"
+                }
+            }
+        ]
     }
 }"""
 
@@ -47,14 +86,9 @@ def lambda_handler(event, context):
     # - Update the values with the parameters
     # - Add new resource to array
     for i, nat_id in enumerate(nat_gateway_ids_list):
+      NatGatewayAlarm = BuildDualMetricAlarm(SamplingPeriod, EvaluationPeriods, DatapointsToAlarm, Threshold)
       ResourceName = f"NATGatewayAlarm{i + 1}"
-      NatGatewayAlarm = json.loads(NatGatewayTemplate)
-      NatGatewayAlarm['Properties']['AlarmName'] = f"NatGateway Alarm for: {nat_id}"
-      NatGatewayAlarm['Properties']['Dimensions'][0]['Value'] = nat_id
-      NatGatewayAlarm['Properties']['Period'] = int(SamplingPeriod)
-      NatGatewayAlarm['Properties']['EvaluationPeriods'] = int(EvaluationPeriods)
-      NatGatewayAlarm['Properties']['DatapointsToAlarm'] = int(DatapointsToAlarm)
-      NatGatewayAlarm['Properties']['Threshold'] = int(Threshold)
+
       # Add the new resource to the resource array
       Resources[ResourceName] = NatGatewayAlarm
 
@@ -64,3 +98,26 @@ def lambda_handler(event, context):
         'status': "success",
         "fragment": fragment
     }
+
+# Load and initialize an alarm with a two metrics
+def BuildDualMetricAlarm(nat_id, SamplingPeriod, EvaluationPeriods, DatapointsToAlarm, Threshold):
+      NatGatewayAlarm = json.loads(NatGatewaySingleMetricTemplate)
+      NatGatewayAlarm['Properties']['AlarmName'] = f"NatGateway Alarm for: {nat_id}"
+      NatGatewayAlarm['Properties']['Dimensions'][0]['Value'] = nat_id
+      NatGatewayAlarm['Properties']['Period'] = int(SamplingPeriod)
+      NatGatewayAlarm['Properties']['EvaluationPeriods'] = int(EvaluationPeriods)
+      NatGatewayAlarm['Properties']['DatapointsToAlarm'] = int(DatapointsToAlarm)
+      NatGatewayAlarm['Properties']['Threshold'] = int(Threshold)
+
+      # Update the Value for Nat ID in the "m1" and "m2" elements
+      NatGatewayAlarm['Properties']['Metrics'][1]['MetricStat']['Metric']['Dimensions'][0]['Value'] = nat_id
+      NatGatewayAlarm['Properties']['Metrics'][2]['MetricStat']['Metric']['Dimensions'][0]['Value'] = nat_id
+
+      # Update the Sampling Period for Nat ID in the "m1" and "m2" elements
+      NatGatewayAlarm['Properties']['Metrics'][1]['MetricStat']['Period'] = int(SamplingPeriod)
+      NatGatewayAlarm['Properties']['Metrics'][2]['MetricStat']['Period'] = int(SamplingPeriod)
+
+      # Update the Threshold value in "Expression" for the "e1" element (the alarm's condition)
+      NatGatewayAlarm['Properties']['Metrics'][0]['Expression'] = f"IF(m1 > {Threshold}, 1, 0) + IF(m2 > {Threshold}, 1, 0)"
+
+      return NatGatewayAlarm  
